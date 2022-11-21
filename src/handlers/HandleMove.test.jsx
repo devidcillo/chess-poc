9f8a86c3dc8ef9297f917c03bd7e1c9563287b04
David <m.david.corrales@gmail.com>
2022-11-21 16:40:41 +0100

    David - fixes move handlerunit tests

--- src/handlers/HandleMove.test.jsx
+++ src/handlers/HandleMove.test.jsx
@@ -3,51 +3,51 @@ import {handleMove} from "./HandleMove";
 describe('Handle Move', function () {
   describe('White Pawn', () => {
     test('should not be able to move backward', () => {
-      expect(() => handleMove({from: 'e2', to: 'e1', piece: 'wP'})).toThrow();
+      expect(handleMove('e2', 'e1', 'wP')).toBe(false);
     })
 
     test('should be able to move forward', () => {
-      expect(() => handleMove({from: 'e2', to: 'e3', piece: 'wP'})).not.toThrow();
+      expect(handleMove('e2','e3','wP')).toBe(true)
     })
 
     test('should be able to move forward 2 spaces when on starting position', () => {
-      expect(() => handleMove({from: 'e2', to: 'e4', piece: 'wP'})).not.toThrow();
+      expect(handleMove('e2','e4','wP')).toBe(true)
     })
   })
 
   describe('Black Pawn', () => {
     test('should not be able to move backward', () => {
-      expect(() => handleMove({from: 'e7', to: 'e8', piece: 'bP'})).toThrow();
+      expect(handleMove('e7','e8','bP')).toBe(false);
     })
 
     test('should be able to move forward', () => {
-      expect(() => handleMove({from: 'e7', to: 'e6', piece: 'bP'})).not.toThrow();
+      expect(handleMove('e7','e6','bP')).toBe(true)
     })
 
     test('should be able to move forward 2 spaces when on starting position', () => {
-      expect(() => handleMove({from: 'e7', to: 'e5', piece: 'bP'})).not.toThrow();
+      expect(handleMove('e7','e5','bP')).toBe(true)
     })
   })
 
   describe('Rooks', () => {
     test('should move forward', () => {
-      expect(() => handleMove({from: 'a1', to: 'a8', piece: 'wR'})).not.toThrow();
-      expect(() => handleMove({from: 'a1', to: 'a8', piece: 'bR'})).not.toThrow();
+      expect(handleMove('a1','a8','wR')).toBe(true)
+      expect(handleMove('a1','a8','bR')).toBe(true)
     })
 
     test('should move backward', () => {
-      expect(() => handleMove({from: 'a8', to: 'a1', piece: 'wR'})).not.toThrow();
-      expect(() => handleMove({from: 'a8', to: 'a1', piece: 'bR'})).not.toThrow();
+      expect(handleMove('a8','a1','wR')).toBe(true)
+      expect(handleMove('a8','a1','bR')).toBe(true)
     })
 
     test('should move left', () => {
-      expect(() => handleMove({from: 'a1', to: 'h1', piece: 'wR'})).not.toThrow();
-      expect(() => handleMove({from: 'a1', to: 'h1', piece: 'bR'})).not.toThrow();
+      expect(handleMove('a1','h1','wR')).toBe(true)
+      expect(handleMove('a1','h1','bR')).toBe(true)
     })
 
     test('should move right', () => {
-      expect(() => handleMove({from: 'h8', to: 'h1', piece: 'wR'})).not.toThrow();
-      expect(() => handleMove({from: 'h8', to: 'h1', piece: 'bR'})).not.toThrow();
+      expect(handleMove('h8','h1','wR')).toBe(true)
+      expect(handleMove('h8','h1','bR')).toBe(true)
     })
   })
 })
